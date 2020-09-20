8eb1ede70e1ef2bb1d9d848472433e7765ffdb50
Sivaprasad <cpp.sivaprasad@gmail.com>
2020-09-20 21:00:29 +0530

    TOH code

--- TOH/main.cpp
+++ TOH/main.cpp
@@ -1,14 +1,26 @@
 //
 //  main.cpp
-//  TOH
+//  TOH - Tower of Hanoi
 //
 //  Created by Sivaprasad Tamatam on 20/09/20.
 //
 
+
+
 #include <iostream>
+using namespace std;
+
+
+void TOH(int n, int A, int B, int C){
+    if(n>0){
+        TOH(n-1, A, C, B);
+        cout<<A<<"      "<<C<<endl;
+        TOH(n-1, B, A, C);
+    }
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    cout<<"From"<<"   "<<"TO"<<endl;
+    TOH(3,1,2,3);
     return 0;
 }
